076465d3ee0b6149ce1ccbf23162b97c6eabfb25
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-16 21:29:54 +0900

    feat: `twitter`,`github`のアイコンを追加

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,6 +1,8 @@
 import { Profile } from '../../types/profile';
 import { eraseTags } from '../../utils/eraseTags';
 import Image from 'next/image';
+import GitHub from '../icons/GitHub';
+import Twitter from '../icons/Twitter';
 
 type Props = {
   profile: Profile;
@@ -12,7 +14,9 @@ const About: React.VFC<Props> = ({ profile }) => {
       <h2>about me</h2>
       <Image src={profile.profile.src} alt='' width={32} height={32} />
       <p>{eraseTags(profile.comment)}</p>
+      <GitHub width={32} height={32} />
       <p>{profile.github}</p>
+      <Twitter width={32} height={32} />
       <p>{profile.twitter}</p>
     </>
   );
